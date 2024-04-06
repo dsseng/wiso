da19f4c5d16bb8b9d7adcc25555a440337032e82
Dmitry Sharshakov <d3dx12.xx@gmail.com>
2024-04-06 10:04:32 +0300

    cli: radusers: add command

--- cmd/radusers.go
+++ cmd/radusers.go
@@ -10,10 +10,6 @@ import (
 	"github.com/spf13/cobra"
 )
 
-func init() {
-	rootCmd.AddCommand(radusersCmd)
-}
-
 var radusersCmd = &cobra.Command{
 	Use:   "radusers",
 	Short: "Manage users in the RADIUS DB (typically MAC addresses)",
@@ -34,4 +30,41 @@ var radusersCmd = &cobra.Command{
 		tbl.Print()
 	},
 	TraverseChildren: true,
+}
+
+var addCmd = &cobra.Command{
+	Use:   "add [MAC]",
+	Short: "Add a MAC address to the RADIUS DB",
+	Args:  cobra.MinimumNArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		var entries []radius.RadCheck
+		res := db.Find(&entries, "username = ?", args[0])
+		if res.Error != nil {
+			fmt.Println("Unable to add row, DB error", res.Error)
+			return
+		} else if res.RowsAffected > 0 {
+			fmt.Println("Unable to add row, already exists")
+			return
+		}
+
+		entry := radius.RadCheck{
+			Username:  args[0],
+			Attribute: "Cleartext-Password",
+			Op:        ":=",
+			Value:     "macauth",
+		}
+		res = db.Create(&entry)
+		if res.Error != nil {
+			fmt.Println(res.Error)
+			return
+		} else {
+			fmt.Println("Added successfully, modified rows:", res.RowsAffected)
+		}
+	},
+	TraverseChildren: true,
+}
+
+func init() {
+	radusersCmd.AddCommand(addCmd)
+	rootCmd.AddCommand(radusersCmd)
 }
