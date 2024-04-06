e6f3f809508d944f30a0409d4847a73f3cb29dc4
Dmitry Sharshakov <d3dx12.xx@gmail.com>
2024-04-06 10:56:02 +0300

    cli: logs: add options to limit output

--- cmd/logs.go
+++ cmd/logs.go
@@ -1,6 +1,9 @@
 package cmd
 
 import (
+	"fmt"
+	"time"
+
 	"github.com/fatih/color"
 	"github.com/rodaine/table"
 
@@ -8,12 +11,23 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var (
+	logsSince string
+	logLines  int
+)
+
 var logsCmd = &cobra.Command{
 	Use:   "logs",
 	Short: "Read authentication logs from the RADIUS database",
 	Run: func(cmd *cobra.Command, args []string) {
 		var results []radius.RadPostAuth
-		db.Find(&results)
+		duration, err := time.ParseDuration(logsSince)
+		if err != nil {
+			fmt.Println("Failed to parse log timeout:", logsSince)
+			return
+		}
+
+		db.Order("authdate DESC").Limit(logLines).Find(&results, "authdate >= ?", time.Now().Add(-duration))
 
 		headerFmt := color.New(color.FgGreen, color.Underline).SprintfFunc()
 		columnFmt := color.New(color.FgYellow).SprintfFunc()
@@ -30,5 +44,7 @@ var logsCmd = &cobra.Command{
 }
 
 func init() {
+	logsCmd.PersistentFlags().StringVar(&logsSince, "since", "168h", "Time to show recent logs for. Valid time units are “ns”, “us”, “ms”, “s”, “m”, “h”")
+	logsCmd.PersistentFlags().IntVarP(&logLines, "lines", "n", 10, "How many lines to show. -1 means displaying all found")
 	rootCmd.AddCommand(logsCmd)
 }
