6c8519e3bd57fb9b1c9bc64b453969e71d98dce5
Dmitry Sharshakov <d3dx12.xx@gmail.com>
2024-04-08 18:41:21 +0300

    web: add OIDC login

--- pkg/web/web.go
+++ pkg/web/web.go
@@ -1,22 +1,51 @@
 package web
 
 import (
+	"context"
 	"embed"
+	"encoding/json"
 	"fmt"
 	"html/template"
 	"net/http"
+	"os"
+	// "strings"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/zitadel/oidc/v3/pkg/client/rp"
+	// httphelper "github.com/zitadel/oidc/v3/pkg/http"
+	"github.com/zitadel/oidc/v3/pkg/oidc"
 	"gorm.io/gorm"
 )
 
 var (
-	db *gorm.DB
 	//go:embed templates
 	embedFS embed.FS
+	db      *gorm.DB
 )
 
 func setupRouter() *gin.Engine {
+	clientID := os.Getenv("CLIENT_ID")
+	clientSecret := os.Getenv("CLIENT_SECRET")
+	issuer := os.Getenv("ISSUER")
+	redirectURI := fmt.Sprintf("http://localhost:%v%v", 8989, "/oidc/callback")
+
+	options := []rp.Option{
+		rp.WithVerifierOpts(rp.WithIssuedAtOffset(5 * time.Second)),
+	}
+	provider, err := rp.NewRelyingPartyOIDC(
+		context.TODO(),
+		issuer,
+		clientID,
+		clientSecret,
+		redirectURI,
+		[]string{"openid", "profile"},
+		options...,
+	)
+	if err != nil {
+		fmt.Printf("error creating provider %s", err.Error())
+	}
+
 	r := gin.Default()
 	r.SetTrustedProxies([]string{})
 	templ := template.Must(
@@ -27,7 +56,12 @@ func setupRouter() *gin.Engine {
 	r.SetHTMLTemplate(templ)
 	staticFS := http.FS(embedFS)
 
-	r.GET("login", func(c *gin.Context) {
+	r.GET("/static/:path", func(c *gin.Context) {
+		fmt.Println(c.Param("path"))
+		c.FileFromFS("templates/static/"+c.Param("path"), staticFS)
+	})
+
+	r.GET("/login", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "login.html", gin.H{
 			"title":        "Network login",
 			"oidcName":     "Gitea",
@@ -35,10 +69,29 @@ func setupRouter() *gin.Engine {
 			"image":        "/static/logo.png",
 		})
 	})
-	r.GET("/static/:path", func(c *gin.Context) {
-		fmt.Println(c.Param("path"))
-		c.FileFromFS("templates/static/"+c.Param("path"), staticFS)
-	})
+
+	marshalUserinfo := func(w http.ResponseWriter, r *http.Request, tokens *oidc.Tokens[*oidc.IDTokenClaims], state string, rp rp.RelyingParty, info *oidc.UserInfo) {
+		data, err := json.Marshal(info)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		fmt.Println(data, state)
+		w.Write(data)
+	}
+
+	// /oidc/login?sess= or ...?mac=
+	r.GET("/oidc/login", gin.WrapF(func(w http.ResponseWriter, r *http.Request) {
+		state := func() string {
+			return r.URL.Query().Get("mac")
+		}
+		(rp.AuthURLHandler(
+			state,
+			provider,
+		))(w, r)
+	}))
+	r.GET("/oidc/callback", gin.WrapF(rp.CodeExchangeHandler(rp.UserinfoCallback(marshalUserinfo), provider)))
 
 	return r
 }
